01effc6a0935b3530e494b32d548f9d0700d0699
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-03 14:59:19 -0700

    add value ex and oo to the clicked box. also add comments to deskribe thought process

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
-// A $( document ).ready() block.
 $(document).ready(function() { //runs jquery when page is ready
   console.log( "ready!" ); //testin if jquery run, fired up ready to go
 
-  var counter = 1; //counter of clicks
+  var counter = 0;  //counter of clicks
+  var X;
+  var O;
     $( ".box" ).one( "click", function(event) {
-      event.preventDefault();
-      if (counter%2===1){
-        $( this ).css( "color", "red");
-      } else if (counter%2===0){
-        $( this ).css('color', 'blue');
+      event.preventDefault(); //prevents double clicking
+      x = (counter%2===1);
+      O =(counter%2===0);
+      if (x){ //first player, aka X
+        $( this ).css( "color", "red"); // add styling to X later
+      } else if (O){//second player, aka O
+        $( this ).css('color', 'blue'); //add styling to O later
       }
-      counter++;
-
+      counter++; //add anoter to counter to keep control over turns
     });
+
 
 });
