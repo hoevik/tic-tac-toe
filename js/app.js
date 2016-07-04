5245da4bf1eeea4b42fb33d759e2edb6f7643054
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-03 18:40:48 -0700

    if winning streak on top row, alert will come

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ $(document).ready(function() { //runs jquery when page is ready
 
     $( ".box" ).one( "click", function(event) {
       //event.preventDefault(); //prevents double clicking
-      x = (counter%2===1);
-      o =(counter%2===0);
+      x = (counter%2===0);
+      o =(counter%2===1);
       if (x){ //first player, aka X
         $( this ).css( "color", 'red');// add styling to X later
         $( this ).val('xx');
@@ -17,8 +17,13 @@ $(document).ready(function() { //runs jquery when page is ready
         $( this ).val('oo');
       }
       counter++;//add anoter to counter to keep control over turns
-      if ($('.rowOne').val()==='oo'){
+
+      if ($('.1').val()==='oo'&& $('.2').val()==='oo'&& $('.3').val()==='oo') {
         window.alert('o wins');
+      } else if($('.1').val()==='xx'&& $('.2').val()==='xx'&& $('.3').val()==='xx'){
+          window.alert('x wins');
       }
+
     });
+
 });
