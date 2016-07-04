f850707e7894201b81faa9c18c09fd813c31e2ce
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-03 18:02:31 -0700

    fixed alert by click working

--- js/app.js
+++ js/app.js
@@ -2,19 +2,23 @@ $(document).ready(function() { //runs jquery when page is ready
   console.log( "ready!" ); //testin if jquery run, fired up ready to go
 
   var counter = 0;  //counter of clicks
-  var X;
-  var O;
+  var x;
+  var o;
+
     $( ".box" ).one( "click", function(event) {
-      event.preventDefault(); //prevents double clicking
+      //event.preventDefault(); //prevents double clicking
       x = (counter%2===1);
-      O =(counter%2===0);
+      o =(counter%2===0);
       if (x){ //first player, aka X
-        $( this ).css( "color", "red"); // add styling to X later
-      } else if (O){//second player, aka O
-        $( this ).css('color', 'blue'); //add styling to O later
+        $( this ).css( "color", 'red');// add styling to X later
+        $( this ).val('xx');
+      } else if (o){//second player, aka O
+        $( this ).css('color', 'blue');//add styling to O later
+        $( this ).val('oo');
       }
-      counter++; //add anoter to counter to keep control over turns
+      counter++;//add anoter to counter to keep control over turns
+      if ($('.rowOne').val()==='oo'){
+        window.alert('o wins');
+      }
     });
-
-
 });
