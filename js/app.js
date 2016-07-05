69f6567076d58e74eafc1d28838505106bfd4562
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-05 09:10:48 -0700

    atempt to make rese button

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@ $(document).ready(function() { //runs jquery when page is ready
       x = (counter%2===0);
       o =(counter%2===1);
       if (x){ //first player, aka X
-        $( this ).css( "color", 'red');// add styling to X later
+        $( this ).css( "color", '.red');// add styling to X later
         $( this ).val('xx');
       } else if (o){//second player, aka O
-        $( this ).css('color', 'blue');//add styling to O later
+        $( this ).css('color', '.blue');//add styling to O later
         $( this ).val('oo');
       }
       counter++;//add anoter to counter to keep control over turns
@@ -25,7 +25,7 @@ $(document).ready(function() { //runs jquery when page is ready
           ($('.3').val()==='oo')&&($('.6').val()==='oo')&&($('.9').val()==='oo')||
           ($('.1').val()==='oo')&&($('.5').val()==='oo')&&($('.9').val()==='oo')||
           ($('.3').val()==='oo')&&($('.5').val()==='oo')&&($('.7').val()==='oo')){
-          return alert('O wins, sorry bruh');
+          return alert('O wins');
         }
       else if(  ($('.1').val()==='xx')&&($('.2').val()==='xx')&&($('.3').val()==='xx')||
                 ($('.4').val()==='xx')&&($('.5').val()==='xx')&&($('.6').val()==='xx')||
@@ -35,21 +35,33 @@ $(document).ready(function() { //runs jquery when page is ready
                 ($('.3').val()==='xx')&&($('.6').val()==='xx')&&($('.9').val()==='xx')||
                 ($('.1').val()==='xx')&&($('.5').val()==='xx')&&($('.9').val()==='xx')||
                 ($('.3').val()==='xx')&&($('.5').val()==='xx')&&($('.7').val()==='xx')){
-                  return alert('X wins, bruh');
+                  return alert('X wins');
                 }
-    else if(counter===9){
-      return alert('Draw, bastards');
-
-    }
-  //to function()
-
-
-/*
-        window.alert('o wins');
-      } else if($('.1').val()==='xx'&& $('.2').val()==='xx'&& $('.3').val()==='xx'){
-          window.alert('x wins');
+      else if(counter===9){
+      return alert('Draw');
       }
-*/
+
+      $('.reset').on("click", function handleClick(e){ // removes, values, and text
+            $('.box').text("");
+            $('.box').val("");
+            // $('.box').css('background-color', "white");
+            turnCounter = 0; 
+          });
+
+
     }); //close function(event)
 
-}); //close document
+    //$( "button" ).on('click', function( reset ){
+
+    //});
+
+
+// var reset = $('button').on('click', function(){
+// $('box:reset');
+
+ });
+
+
+
+
+ //close document
